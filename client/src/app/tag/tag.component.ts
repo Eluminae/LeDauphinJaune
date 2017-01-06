6ec2ee54e2629cd34cc10539458dbcbe6165267c
MisterBanal <eddiebarraco@live.fr>
2017-01-06 09:34:58 +0100

    Renamned tag.css to tag.scss. Oops...

--- client/src/app/tag/tag.component.ts
+++ client/src/app/tag/tag.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html',
-  styleUrls: ['./tag.component.css']
+  styleUrls: ['./tag.component.scss']
 })
 export class TagComponent implements OnInit {
 
